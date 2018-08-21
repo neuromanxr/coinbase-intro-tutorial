c73390a1ca3a05efa483809d9641dc2e85b50897
Kelvin Lee <kelvin.lee@newsela.com>
2018-08-20 23:58:31 -0400

    Initial Commit

---
+++ basic-coinbase-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  basic-coinbase-tutorial
+//
+//  Created by Kelvin Lee on 8/20/18.
+//  Copyright © 2018 Kelvin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
